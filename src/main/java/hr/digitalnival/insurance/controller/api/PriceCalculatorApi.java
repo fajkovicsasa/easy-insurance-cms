4bf2759669f016b48b5a4c6179f031150d72425f
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-14 21:15:14 +0200

    Added jUnit dependency

---
+++ src/main/java/hr/digitalnival/insurance/controller/api/PriceCalculatorApi.java
@@ -0,0 +1,9 @@
+package hr.digitalnival.insurance.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/price-calculator/v1")
+public class PriceCalculatorApi {
+}
