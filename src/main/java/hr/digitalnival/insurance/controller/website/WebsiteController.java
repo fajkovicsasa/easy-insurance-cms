c2e923153a3ddffacf4212ca64483067d27bfca7
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-15 22:19:46 +0200

    Corrected thymeleaf routes

--- src/main/java/hr/digitalnival/insurance/controller/website/WebsiteController.java
+++ src/main/java/hr/digitalnival/insurance/controller/website/WebsiteController.java
@@ -30,14 +30,14 @@ public class WebsiteController {
         return "index";
     }
 
-    @GetMapping("/admin")
+    @GetMapping("admin")
     public String showAdminDashboard(Model model, @RequestParam(value = "error", required = false) Boolean showError) {
         List<InsuranceType> insuranceTypes = insuranceTypeService.getAll();
 
         model.addAttribute("error", showError);
         model.addAttribute("allInsuranceTypes", insuranceTypes);
 
-        return "/admin/dashboard";
+        return "admin/dashboard";
     }
 
     @GetMapping("/admin/insurance-types/{id}")
@@ -45,7 +45,7 @@ public class WebsiteController {
         log.info("Opening update for insurance type " + id);
         model.addAttribute("insuranceType", insuranceTypeService.getInsuranceType(id));
 
-        return "/admin/insuranceTypeDetails";
+        return "admin/insuranceTypeDetails";
     }
 
     @PostMapping("/admin/insurance-types/{id}")
@@ -64,14 +64,14 @@ public class WebsiteController {
             model.addAttribute("insuranceType", insuranceTypeService.getInsuranceType(id));
         }
 
-        return "/admin/insuranceTypeDetails";
+        return "admin/insuranceTypeDetails";
     }
 
     @GetMapping("/admin/insurance-types/new")
     public String createNewInsuranceType() {
         log.debug("Creating a new insurance type");
 
-        return "/admin/newInsuranceType";
+        return "admin/newInsuranceType";
     }
 
     @PostMapping("/admin/insurance-types/new")
